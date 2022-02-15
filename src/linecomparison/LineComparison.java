f7160ee10c7f6aa5f82cd016a788193723f17c6b
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-16 01:41:21 +0530

    uc1 is done

---
+++ src/linecomparison/LineComparison.java
@@ -0,0 +1,32 @@
+package linecomparison;
+
+import java.util.Scanner;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program on CSE");
+		
+        Scanner sc = new Scanner(System.in);
+
+		  System.out.println("Enter the Co-ordinates of x1 and y1 : ");
+		  
+        int x1 = sc.nextInt();
+        
+        int y1 = sc.nextInt();
+        
+        System.out.println("Enter the Co-ordinates of x2 and y2 : ");
+        
+        int x2 = sc.nextInt();
+        
+        int y2 = sc.nextInt();
+        
+        double len = Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1));
+        
+        System.out.println("Length of the given Line is : " + len + " units" );
+        
+    }
+}
+
+	
